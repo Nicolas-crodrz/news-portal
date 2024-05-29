e39656d731a2b6961da6d74e0eea8ee779206911
Nico <cabrerarodriguezftv@gmail.com>
2024-05-29 07:12:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
