ed50e8782243a7a29887e2107e0d7f8dc8e58559
Nicolas-Cabrera-Rodriguez <cabrerarodriguezftv@gmail.com>
2024-06-04 09:53:26 +0100

    Styles error Fixed

--- app/Policies/NewsPolicy.php
+++ app/Policies/NewsPolicy.php
@@ -3,35 +3,38 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\News;
 
 class NewsPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new policy instance.
+   */
+  public function __construct()
+  {
+    //
+  }
 
-    public function before($user, $ability)
-{
+  public function before($user, $ability)
+  {
+
     if ($user->role->name === 'Administrador') {
-        return true; // Permite el acceso para Administradores
+      return true; // Permite el acceso para Administradores
     }
+  }
 
-    public function create(User $user)
-{
-    return $user->role->name === 'Administrador' || $user->role->name === 'Editor';
-}
+  public function view(User $user, News $news)
+  {
 
-public function edit(User $user, News $news)
-{
     return $user->role->name === 'Administrador' || ($user->role->name === 'Editor' && $news->user_id === $user->id);
-}
+  }
 
-// Otros métodos según sea necesario
+  public function edit(User $user, News $news)
+  {
 
-}
+    return $user->role->name === 'Administrador' || ($user->role->name === 'Editor' && $news->user_id === $user->id);
+  }
+
+  // Otros métodos según sea necesario
 
 }
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,37 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto px-4">
-    <h1 class="text-2xl font-bold mb-4">Noticias</h1>
-    <a href="{{ route('news.create') }}" class="text-black">Crear Noticia</a>
-    <a href="{{ route('news.create') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-4 rounded"> Crear Noticia</a>
-    <a href="{{ route('news.create') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-black font-bold py-2 px-4 rounded shadow !important">
-        Crear Noticia
-    </a>
-    
-    
+    <div class="container mx-auto px-4">
+        <h1 class="text-2xl font-bold mb-4">Noticias</h1>
+        <a href="{{ route('news.create') }}" class="">Crear Noticia</a>
 
-    @foreach($news as $newsItem)
-        <div class="bg-white shadow-md rounded my-4 p-4">
-            <h2 class="text-xl font-semibold">{{ $newsItem->title }}</h2>
-            <!-- Agregar imagen si existe -->
-            @if ($newsItem->hasMedia('images'))
-                <img src="{{ $newsItem->getFirstMediaUrl('images') }}" alt="{{ $newsItem->title }}" class="my-2 w-full h-auto">
-            @endif
-            <!-- Mostrar solo parte del contenido -->
-            <p>{{ Str::limit($newsItem->content, 150) }}</p>
-            <!-- Agregar enlace para leer la noticia completa -->
-            <a href="{{ route('news.show', $newsItem->id) }}" class="text-blue-500">Leer más</a>
-            <!-- Agregar botón de editar y formulario de eliminar -->
-            <div class="mt-4">
-                <a href="{{ route('news.edit', $newsItem->id) }}" class="text-blue-500">Editar</a>
-                <form action="{{ route('news.destroy', $newsItem->id) }}" method="POST" class="inline">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="text-red-500 ml-4">Eliminar</button>
-                </form>
+
+
+        @foreach ($news as $newsItem)
+            <div class="bg-white shadow-md rounded my-4 p-4">
+                <h2 class="text-xl font-semibold">{{ $newsItem->title }}</h2>
+                <!-- Agregar imagen si existe -->
+                @if ($newsItem->hasMedia('images'))
+                    <img src="{{ $newsItem->getFirstMediaUrl('images') }}" alt="{{ $newsItem->title }}"
+                        class="my-2 w-full h-auto">
+                @endif
+                <!-- Mostrar solo parte del contenido -->
+                <p>{{ Str::limit($newsItem->content, 150) }}</p>
+                <!-- Agregar enlace para leer la noticia completa -->
+                <a href="{{ route('news.show', $newsItem->id) }}" class="text-blue-500">Leer más</a>
+                <!-- Agregar botón de editar y formulario de eliminar -->
+                <div class="mt-4">
+                    <a href="{{ route('news.edit', $newsItem->id) }}" class="text-blue-500">Editar</a>
+                    <form action="{{ route('news.destroy', $newsItem->id) }}" method="POST" class="inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-red-500 ml-4">Eliminar</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
 @endsection
