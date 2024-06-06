6361b9d5f7dedb7030f7ff5dc1df272399eac0d4
Nicolas-Cabrera-Rodriguez <cabrerarodriguezftv@gmail.com>
2024-06-06 08:35:48 +0100

    fixed #7

--- resources/views/news/edit.blade.php
+++ resources/views/news/edit.blade.php
@@ -15,7 +15,7 @@
             <div class="mb-6">
                 <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Contenido</label>
                 <textarea name="content" id="content"
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ $news->content }}</textarea>
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ strip_tags($news->content) }}</textarea>
             </div>
             <button type="submit"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Actualizar</button>
